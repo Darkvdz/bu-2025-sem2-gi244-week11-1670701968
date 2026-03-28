c590020386c841ce1b00caafaf238183612ba507
Darkvdz <famparintorn@gmail.com>
2026-03-29 01:59:56 +0700

    Second_Finished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -5,6 +6,8 @@ public class Enemy : MonoBehaviour
     public float speed = 3f;
     private Rigidbody rb;
     private GameObject player;
+    
+    private bool isStunned = false;
 
     void Start()
     {
@@ -16,10 +19,19 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isStunned) return;
+        
         Vector3 dir = player.transform.position - transform.position;
         dir.Normalize();
         rb.AddForce(dir * speed);
-        
-
     }
+
+    public IEnumerator StunRoutine(float duration)
+    {
+        isStunned = true;
+        rb.linearVelocity = Vector3.zero;
+        yield return new WaitForSeconds(duration);
+        isStunned = false;
+    }
+    
 }
---
+++ Assets/Scripts/StunPowerUp.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class StunPowerUp : MonoBehaviour
+{
+    public float stunDuration = 5.0f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject [] allenemis = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach (GameObject enemyObj in allenemis)
+            {
+                Enemy enemyScript = enemyObj.GetComponent<Enemy>();
+                if (enemyScript != null)
+                {
+                    enemyScript.StartCoroutine(enemyScript.StunRoutine(stunDuration));
+
+                }
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
